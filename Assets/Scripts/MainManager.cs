102040b2e43e7699d599d5fc036c3fcbc3264599
Tarik-Pisiren <t.pisiren@icloud.com>
2025-02-17 19:09:18 +0300

    added MenuUIHandler.cs

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -6,26 +6,30 @@ using UnityEngine.UI;
 
 public class MainManager : MonoBehaviour
 {
+
+
+
+
     public Brick BrickPrefab;
     public int LineCount = 6;
     public Rigidbody Ball;
-
     public Text ScoreText;
     public GameObject GameOverText;
-    
     private bool m_Started = false;
     private int m_Points;
-    
     private bool m_GameOver = false;
 
-    
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
         const float step = 0.6f;
         int perLine = Mathf.FloorToInt(4.0f / step);
-        
-        int[] pointCountArray = new [] {1,1,2,2,5,5};
+
+        int[] pointCountArray = new[] { 1, 1, 2, 2, 5, 5 };
         for (int i = 0; i < LineCount; ++i)
         {
             for (int x = 0; x < perLine; ++x)
@@ -37,6 +41,12 @@ public class MainManager : MonoBehaviour
             }
         }
     }
+
+
+
+
+
+
 
     private void Update()
     {
@@ -62,15 +72,33 @@ public class MainManager : MonoBehaviour
         }
     }
 
+
+
+
+
+
+
     void AddPoint(int point)
     {
         m_Points += point;
         ScoreText.text = $"Score : {m_Points}";
     }
 
+
+
+
+
+
+
     public void GameOver()
     {
         m_GameOver = true;
         GameOverText.SetActive(true);
     }
+
+
+
+
+
+
 }
